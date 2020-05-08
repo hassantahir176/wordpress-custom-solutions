08269c0e48b91aa6afa2c31ad5a54b23915e9878
hassantahir176 <64250906+hassantahir176@users.noreply.github.com>
2020-05-08 12:33:43 +0500

    Update filter-orders-based-on-custom-fields.php

--- filter-orders-based-on-custom-fields.php
+++ filter-orders-based-on-custom-fields.php
@@ -23,3 +23,41 @@ function filter_orders_by_store_query($vars){
 	}
 	return $vars;
 }
+
+//Following is the second example of adding custom filters
+
+add_action( 'restrict_manage_posts', 'filter_orders_by_order_creator', 20 );
+function filter_orders_by_order_creator() {
+	global $typenow;
+	if ( 'shop_order' === $typenow ) {
+		// get all payment methods, even inactive ones
+		//$gateways = WC()->payment_gateways->payment_gateways();
+                
+                global $wpdb;
+		$order_creators= $wpdb->get_results("Select ID, user_login from wp_users where ID IN (Select DISTINCT meta_value FROM wp_postmeta WHERE meta_key=\"_wpo_order_creator\")");
+
+	?>
+		<select name="mht_order_creator_filter">
+			<option value="">All Order Creators</option>
+<?php                   
+                        if($order_creators){
+                            foreach($order_creators as $order_creator){
+                        ?>
+                                <option value="<?php echo $order_creator->ID; ?>"><?php echo $order_creator->user_login; ?></option>
+<?php
+                            }
+                        }
+?>
+		</select>
+	<?php
+	}
+}
+add_filter( 'request', 'filter_orders_by_order_creator_query' );
+function filter_orders_by_order_creator_query($vars){
+	global $typenow;
+	if ( 'shop_order' === $typenow && isset( $_GET['mht_order_creator_filter'] ) && ! empty( $_GET['mht_order_creator_filter'] ) ) {
+		$vars['meta_key']   = '_wpo_order_creator';
+		$vars['meta_value'] = wc_clean( $_GET['mht_order_creator_filter'] );
+	}
+	return $vars;
+}
